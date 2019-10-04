559d3b76e8813cfa49509fad1ef313d25355e81e
Daniel Martins <daniel.tritone@gmail.com>
2019-10-04 10:01:25 -0300

    Allow sorting by track #

--- spotify-track-search.el
+++ spotify-track-search.el
@@ -166,7 +166,7 @@ be played in the context of its album."
   "Configures the column data for the typical track view."
   (let ((default-width (truncate (/ (- (window-width) 30) 3))))
     (setq tabulated-list-format
-          (vector '("#" 3 nil :right-align t)
+          (vector '("#" 3 t :right-align t)
                   `("Track Name" ,default-width t)
                   `("Artist" ,default-width t)
                   `("Album" ,default-width t)
