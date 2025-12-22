3a0657580feab7dd4dd4ed0ddc84edc12e410a93
Daniel Martins <email@danielfm.me>
2025-12-22 13:13:26 -0300

    Update mode doc string

--- smudge.el
+++ smudge.el
@@ -16,7 +16,7 @@
 
 ;; Before using this mode, first go the Spotify Web API console
 ;; <https://developer.spotify.com/my-applications> and create a new application,
-;; adding <http://localhost:8080/smudge-callback> as the redirect URI (or
+;; adding <http://127.0.0.1:8080/smudge_api_callback> as the redirect URI (or
 ;; whichever port you have specified via customize).
 
 ;; After requiring `smudge', make sure to define the client id and client
